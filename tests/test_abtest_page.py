6ee02d8b2a8c54951ce680a123a7ce098cbb1623
R <GarereGari251243@proton.me>
2023-01-27 01:58:37 +0800

    Changed test_abtest_page.py

--- tests/test_abtest_page.py
+++ tests/test_abtest_page.py
@@ -1,15 +1,29 @@
-import time
-
 from pages.abtest_page import Abtest_page
 
+ABTEST_TITLE_TEXT = "A/B Test"
+ABTEST_PART_OF_TEXT = "learn different versions"
 
-def test_abtest_page_should_work():
-    page = Abtest_page()
+
+def test_abtest_page(driver) -> None:
+    page = Abtest_page(driver)
     page.navigate_to_abtest_page()
-    time.sleep(1)
 
-    assert page.check_page() is True
-    assert "A/B Test" in page.check_title()
-    assert "learn different versions" in page.check_text()
+    assert page.at_page()
 
 
+def test_abtest_page_title(driver) -> None:
+    page = Abtest_page(driver)
+    page.navigate_to_abtest_page()
+
+    assert page.at_page()
+    assert page.get_title().is_displayed()
+    assert ABTEST_TITLE_TEXT in page.get_title_text()
+
+
+def test_abtest_page_text(driver) -> None:
+    page = Abtest_page(driver)
+    page.navigate_to_abtest_page()
+
+    assert page.at_page()
+    assert page.get_content().is_displayed()
+    assert ABTEST_PART_OF_TEXT in page.get_content_text()
