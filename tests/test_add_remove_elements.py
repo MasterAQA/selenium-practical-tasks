21cfcc874427c0c5fa693f800c45ef827ac1fb79
R <GarereGari251243@proton.me>
2023-01-27 01:59:00 +0800

    Changed test_add_remove_elements.py

--- tests/test_add_remove_elements.py
+++ tests/test_add_remove_elements.py
@@ -2,17 +2,18 @@ import time
 
 from pages.add_remove_elements_page import Add_remove_elements_page
 
+ADD_REMOVE_ELEMENTS_TITLE = "Add/Remove Elements"
+
 
 def test_add_remove_elements_page_should_work():
     page = Add_remove_elements_page()
     page.navigate_to_add_remove_elements_page()
 
     page.add_element()
-    page.add_elements(5)
+    [page.add_element() for _ in range(5)]
 
     page.remove_element()
-    page.remove_elements(5)
-    time.sleep(1)
+    [page.add_element() for _ in range(5)]
 
-    assert page.check_page() is True
-    assert page.check_title() == "Add/Remove Elements"
+    assert page.at_page()
+    assert page.get_title_text() == ADD_REMOVE_ELEMENTS_TITLE
