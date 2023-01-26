2b76b4672eef83b424284d8138b71c8defade970
R <GarereGari251243@proton.me>
2023-01-27 01:58:25 +0800

    Changed base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,26 +1,21 @@
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium import webdriver
-from tests.settings import *
+
 
 class Base_page:
-
-    def __init__(self):
-        self.driver = webdriver.Firefox()
+    def __init__(self, driver):
+        self._driver = driver
+        self.default_url = "https://the-internet.herokuapp.com"
         self.get_url()
 
+    def get_url(self) -> None:
+        self._driver.get(self.default_url)
 
-    def get_url(self):
-        self.driver.get(default_url)
+    def wait_for(self, *locator: str) -> WebDriver:
+        return WebDriverWait(self._driver, 10).until(
+            ec.presence_of_element_located(locator)
+        )
 
-    def wait_for(self, *locator):
-        return WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(locator))
-
-    def check_page_base(self, link_end):
-        if (self.driver.current_url) == f"{default_url+link_end}":
-            return True
-        else:
-            return False
-
-    def __del__(self):
-        self.driver.close()
+    def at_page_base(self, link_end: str) -> bool:
+        return (self._driver.current_url) == f"{self.default_url+link_end}"
