2bdbeb3dff8b9872e3839cb0268419a2cf074395
R <ForUiPathacc94@proton.me>
2023-01-28 00:34:47 +0800

    Updated tests inside

--- tests/test_add_remove_elements.py
+++ tests/test_add_remove_elements.py
@@ -3,15 +3,36 @@ from pages.add_remove_elements_page import AddRemoveElementsPage
 ADD_REMOVE_ELEMENTS_TITLE = "Add/Remove Elements"
 
 
-def test_add_remove_elements_page_should_work():
-    page = AddRemoveElementsPage()
-    page.navigate_to_add_remove_elements_page()
+def test_add_remove_elements_page(add_remove_elements_page):
+    add_remove_elements_page.navigate_to_page()
 
-    page.add_element()
-    [page.add_element() for _ in range(5)]
+    assert add_remove_elements_page.at_page()
 
-    page.remove_element()
-    [page.add_element() for _ in range(5)]
 
-    assert page.at_page()
-    assert page.get_title_text() == ADD_REMOVE_ELEMENTS_TITLE
+
+def test_add_remove_elements_page_title(add_remove_elements_page):
+    add_remove_elements_page.navigate_to_page()
+
+    assert add_remove_elements_page.at_page()
+    assert add_remove_elements_page.get_title_text() == ADD_REMOVE_ELEMENTS_TITLE
+
+
+def test_add_remove_elements_page_add_elements(add_remove_elements_page):
+    add_remove_elements_page.navigate_to_page()
+
+    [add_remove_elements_page.add_element() for _ in range(5)]
+
+    assert add_remove_elements_page.at_page()
+    assert add_remove_elements_page.check_elements_count() == 5
+
+def test_add_remove_elements_page_remove_elements(add_remove_elements_page):
+    add_remove_elements_page.navigate_to_page()
+
+    [add_remove_elements_page.add_element() for _ in range(5)]
+    [add_remove_elements_page.remove_element() for _ in range(4)]
+
+    assert add_remove_elements_page.at_page()
+    assert add_remove_elements_page.check_elements_count() == 1
+
+
+
