b4de4a9b4f687633205084a51a38fe8da5a48462
R <foruipathacc94@proton.me>
2023-01-28 06:41:32 +0800

    Added context_menu_page.py and test_context_menu.py

---
+++ pages/context_menu_page.py
@@ -0,0 +1,39 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+from selenium.webdriver.remote.webdriver import WebDriver
+import requests
+
+
+class ContextMenu(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+        self.actionChains = ActionChains(driver)
+
+    _CONTEXT_MENU = (By.LINK_TEXT, "Context Menu")
+    _CONTEXT_MENU_TITLE = (By.TAG_NAME, "h3")
+    _CONTEXT_MENU_ZONE = (By.XPATH, "//div[@id='hot-spot']")
+
+    def navigate_to_page(self):
+        self.wait_for(*self._CONTEXT_MENU).click()
+
+    def get_title_text(self) -> str:
+        return self.wait_for(*self._CONTEXT_MENU_TITLE).text
+
+    def right_click_on_context_zone(self):
+        self.actionChains.context_click(
+            self.wait_for(*self._CONTEXT_MENU_ZONE)
+        ).perform()
+
+    def alert_text(self) -> str:
+        return self.driver.switch_to.alert.text
+
+    def alert_accept(self):
+        self.driver.switch_to.alert.accept()
+
+    def alert_dismiss(self):
+        self.driver.switch_to.alert.dismiss()
+
+    def at_page(self) -> bool:
+        return self.at_page_base("/context_menu")
---
+++ tests/test_context_menu.py
@@ -0,0 +1,45 @@
+import time
+
+import pytest
+
+CONTEXT_MENU_TITLE_TEXT = "Context Menu"
+ALERT_TEXT = "You selected a context menu"
+
+
+def test_context_menu(context_menu_page) -> None:
+    context_menu_page.navigate_to_page()
+
+    assert context_menu_page.at_page()
+
+
+def test_context_menu_title_text(context_menu_page) -> None:
+    context_menu_page.navigate_to_page()
+
+    assert context_menu_page.at_page()
+    assert context_menu_page.get_title_text() == CONTEXT_MENU_TITLE_TEXT
+
+
+def test_context_menu_alert_text(context_menu_page) -> None:
+    context_menu_page.navigate_to_page()
+    context_menu_page.right_click_on_context_zone()
+
+    assert context_menu_page.alert_text() == ALERT_TEXT
+    context_menu_page.alert_accept()
+
+
+def test_context_menu_alert_accept(context_menu_page) -> None:
+    context_menu_page.navigate_to_page()
+    context_menu_page.right_click_on_context_zone()
+    context_menu_page.alert_accept()
+
+    assert context_menu_page.at_page()
+    assert context_menu_page.get_title_text() == CONTEXT_MENU_TITLE_TEXT
+
+
+def test_context_menu_alert_dismiss(context_menu_page) -> None:
+    context_menu_page.navigate_to_page()
+    context_menu_page.right_click_on_context_zone()
+    context_menu_page.alert_dismiss()
+
+    assert context_menu_page.at_page()
+    assert context_menu_page.get_title_text() == CONTEXT_MENU_TITLE_TEXT
