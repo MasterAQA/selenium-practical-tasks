afb4d8015d77bc59fcccb94e6aadde19b92de80f
R <foruipathacc94@proton.me>
2023-01-28 00:20:11 +0800

    Added BasicAuth in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from pages.abtest_page import AbtestPage
+from pages.add_remove_elements_page import AddRemoveElementsPage
+from pages.basic_auth_page import BasicAuthPage
 
 @pytest.fixture()
 def driver():
@@ -13,3 +15,12 @@ def driver():
 @pytest.fixture()
 def abtest_page(driver) -> AbtestPage:
     return AbtestPage(driver=driver)
+
+@pytest.fixture()
+def add_remove_elements_page(driver) -> AddRemoveElementsPage:
+    return AddRemoveElementsPage(driver=driver)
+
+@pytest.fixture()
+def basic_auth_page(driver) -> BasicAuthPage:
+    return BasicAuthPage(driver=driver)
+
