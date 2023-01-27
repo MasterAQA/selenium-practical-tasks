ef70cbcafa3ce2b3144e3321e88f998f472f1eae
R <foruipathacc94@proton.me>
2023-01-28 06:43:53 +0800

    Added new fixtures to conftest.py to call classes

--- conftest.py
+++ conftest.py
@@ -1,8 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+
 from pages.abtest_page import AbtestPage
 from pages.add_remove_elements_page import AddRemoveElementsPage
 from pages.basic_auth_page import BasicAuthPage
+from pages.broken_images_page import BrokenImagesPage
+from pages.checkbox_page import CheckboxPage
+from pages.context_menu_page import ContextMenu
+
 
 @pytest.fixture()
 def driver():
@@ -16,11 +22,27 @@ def driver():
 def abtest_page(driver) -> AbtestPage:
     return AbtestPage(driver=driver)
 
+
 @pytest.fixture()
 def add_remove_elements_page(driver) -> AddRemoveElementsPage:
     return AddRemoveElementsPage(driver=driver)
+
 
 @pytest.fixture()
 def basic_auth_page(driver) -> BasicAuthPage:
     return BasicAuthPage(driver=driver)
 
+
+@pytest.fixture()
+def broken_images_page(driver) -> BrokenImagesPage:
+    return BrokenImagesPage(driver=driver)
+
+
+@pytest.fixture()
+def checkbox_page(driver) -> CheckboxPage:
+    return CheckboxPage(driver=driver)
+
+
+@pytest.fixture()
+def context_menu_page(driver) -> ContextMenu:
+    return ContextMenu(driver=driver)
