84dfb478362a4c685811e47bb681ed7499621983
R <foruipathacc94@proton.me>
2023-01-28 00:02:33 +0800

    Added test_basic_auth.py

---
+++ tests/test_basic_auth.py
@@ -0,0 +1,26 @@
+import time
+
+import pytest
+
+BASIC_AUTH_TITLE_TEXT = "Basic Auth"
+PASS_BASIC_AUTH_PART_OF_TEXT = "Congratulations!"
+
+
+def test_basic_auth_page_pass_auth(basic_auth_page) -> None:
+    basic_auth_page.pass_basic_auth("admin", "admin")
+    basic_auth_page.navigate_to_page()
+
+    assert basic_auth_page.at_page_admin()
+
+def test_basic_auth_page_title_and_content(basic_auth_page) -> None:
+    basic_auth_page.pass_basic_auth("admin", "admin")
+    basic_auth_page.navigate_to_page()
+
+    assert basic_auth_page.at_page_admin()
+    assert basic_auth_page.get_title_text() == BASIC_AUTH_TITLE_TEXT
+    assert PASS_BASIC_AUTH_PART_OF_TEXT in basic_auth_page.get_content_text()
+
+
+
+
+
