bb166b3a134b72274ebd6df26b8e3120b97a4b52
R <foruipathacc94@proton.me>
2023-01-30 18:11:13 +0800

    Changed values

--- pages/horizontal_slider_page.py
+++ pages/horizontal_slider_page.py
@@ -21,10 +21,13 @@ class HorizontalSliderPage(BasePage):
         return self.wait_for(*self._HORIZONTAL_SLIDER_TITLE).text
 
     def move_slider(self, percent):
-        width = self.wait_for(*self._SLIDER).size['width']
-        one_percent = (width/100)
+        width = self.wait_for(*self._SLIDER).size["width"]
+        one_percent = width / 100
         self.actionChains.drag_and_drop_by_offset(
-            self.find_element(*self._SLIDER), (((percent*10)*one_percent)/0.5)-(one_percent*50), 0).perform()
+            self.find_element(*self._SLIDER),
+            (((percent * 10) * one_percent) / 0.5) - (one_percent * 50),
+            0,
+        ).perform()
 
     def slider_value(self) -> float:
         return float(self.wait_for(*self._SLIDER_VALUE).text)
--- tests/test_horizontal_slider.py
+++ tests/test_horizontal_slider.py
@@ -4,20 +4,19 @@ import pytest
 
 HORIZONTAL_SLIDER_TITLE_TEXT = "Horizontal Slider"
 
-# AFTER_REMOVE_BUTTON = "It's gone!"
-# AFTER_ADD_BUTTON = "It's back!"
-
 
 def test_horizontal_slider(horizontal_slider_page) -> None:
     horizontal_slider_page.navigate_to_page()
 
     assert horizontal_slider_page.at_page()
 
+
 def test_horizontal_slider_title_text(horizontal_slider_page) -> None:
     horizontal_slider_page.navigate_to_page()
 
     assert horizontal_slider_page.at_page()
     assert horizontal_slider_page.get_title_text() == HORIZONTAL_SLIDER_TITLE_TEXT
+
 
 def test_horizontal_slider_move(horizontal_slider_page) -> None:
     horizontal_slider_page.navigate_to_page()
@@ -26,6 +25,7 @@ def test_horizontal_slider_move(horizontal_slider_page) -> None:
     assert horizontal_slider_page.at_page()
     assert horizontal_slider_page.slider_value() == 0.5
 
+
 def test_horizontal_slider_move_(horizontal_slider_page) -> None:
     horizontal_slider_page.navigate_to_page()
     horizontal_slider_page.move_slider(4)
@@ -33,10 +33,10 @@ def test_horizontal_slider_move_(horizontal_slider_page) -> None:
     assert horizontal_slider_page.at_page()
     assert horizontal_slider_page.slider_value() == 4
 
+
 def test_horizontal_slider_move_max(horizontal_slider_page) -> None:
     horizontal_slider_page.navigate_to_page()
     horizontal_slider_page.move_slider(6)
 
     assert horizontal_slider_page.at_page()
     assert horizontal_slider_page.slider_value() == 5
-
