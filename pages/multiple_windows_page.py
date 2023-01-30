150c76dd47afe56890f3b663a32e090e598d226f
R <foruipathacc94@proton.me>
2023-01-30 18:12:40 +0800

    Added multiple_windows_page.py and test_multiple_windows.py

---
+++ pages/multiple_windows_page.py
@@ -0,0 +1,42 @@
+import time
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.select import Select
+
+from pages.base_page import BasePage
+
+
+class MultipleWindowsPage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    _MULTIPLE_WINDOWS = (By.LINK_TEXT, "Multiple Windows")
+    _MULTIPLE_WINDOWS_TITLE = (By.TAG_NAME, "h3")
+    _LINK_ON_NEW_WINDOW = (By.XPATH, "//div[@class='example']/a")
+
+    def navigate_to_page(self):
+        self.wait_for(*self._MULTIPLE_WINDOWS).click()
+
+    def get_title_text(self) -> str:
+        return self.wait_for(*self._MULTIPLE_WINDOWS_TITLE).text
+
+    def window_handle_by_index(self, index: int):
+        return self.driver.window_handles[index]
+
+    def switch_window(self, index_windows_handle):
+        self.driver.switch_to.window(index_windows_handle)
+
+    def new_window(self):
+        self.wait_for(*self._LINK_ON_NEW_WINDOW).click()
+
+    def size_handles(self) -> int:
+        return len(self.driver.window_handles)
+
+    def at_page(self) -> bool:
+        return self.at_page_base("/windows")
+
+    def at_new_page(self) -> bool:
+        return self.at_page_base("/windows/new")
---
+++ tests/test_multiple_windows.py
@@ -0,0 +1,58 @@
+import time
+
+import pytest
+
+MULTIPLE_WINDOWS_TITLE_TEXT = "Opening a new window"
+NEW_WINDOW_TEXT = "New Window"
+SIZE_HANDLES_2 = 2
+SIZE_HANDLES_3 = 3
+
+
+def test_multiple_windows(multiple_windows_page) -> None:
+    multiple_windows_page.navigate_to_page()
+
+    assert multiple_windows_page.at_page()
+
+
+def test_multiple_windows_title_text(multiple_windows_page) -> None:
+    multiple_windows_page.navigate_to_page()
+
+    assert multiple_windows_page.at_page()
+    assert multiple_windows_page.get_title_text() == MULTIPLE_WINDOWS_TITLE_TEXT
+
+
+def test_multiple_windows_new_window(multiple_windows_page) -> None:
+    multiple_windows_page.navigate_to_page()
+    multiple_windows_page.new_window()
+    two_window = multiple_windows_page.window_handle_by_index(1)
+    multiple_windows_page.switch_window(two_window)
+
+    assert multiple_windows_page.get_title_text() == NEW_WINDOW_TEXT
+    assert multiple_windows_page.at_new_page()
+
+
+def test_multiple_windows_new_window_and_return(multiple_windows_page) -> None:
+    multiple_windows_page.navigate_to_page()
+    first_window = multiple_windows_page.window_handle_by_index(0)
+    multiple_windows_page.new_window()
+    two_window = multiple_windows_page.window_handle_by_index(1)
+    multiple_windows_page.switch_window(first_window)
+
+    assert multiple_windows_page.at_page()
+    assert multiple_windows_page.get_title_text() == MULTIPLE_WINDOWS_TITLE_TEXT
+    assert multiple_windows_page.size_handles() == SIZE_HANDLES_2
+
+
+def test_multiple_windows_new_windows(multiple_windows_page) -> None:
+    multiple_windows_page.navigate_to_page()
+    first_window = multiple_windows_page.window_handle_by_index(0)
+    multiple_windows_page.new_window()
+    two_window = multiple_windows_page.window_handle_by_index(1)
+    multiple_windows_page.switch_window(first_window)
+    multiple_windows_page.new_window()
+    three_window = multiple_windows_page.window_handle_by_index(2)
+    multiple_windows_page.switch_window(three_window)
+
+    assert multiple_windows_page.get_title_text() == NEW_WINDOW_TEXT
+    assert multiple_windows_page.at_new_page()
+    assert multiple_windows_page.size_handles() == SIZE_HANDLES_3
